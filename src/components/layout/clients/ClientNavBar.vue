3cae7b9d0151e2952e201e3497627def335214d9
kecoco <kecoco16@gmail.com>
2017-12-14 11:27:44 -0600

    Agregando boton de signout

--- src/components/layout/clients/ClientNavBar.vue
+++ src/components/layout/clients/ClientNavBar.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
   nav.navbar-inverse.navbar-fixed-top.animated.fadeIn
     .row.container-fluid
-      .col-xs-3.col-sm-4.col-md-5
+      .col-xs-3.col-sm-5.col-md-5
         router-link(to="/home")
           a.navbar-brand
             i.fa.fa-home.fa-fw
             strong.hidden-xs &nbsp Inicio 
-      .row.centrar.col-xs-6.col-sm-4.col-md-2.text-center
+      .row.centrar.col-xs-6.col-sm-2.col-md-2.text-center
         input#buscar.form-control(placeholder='Buscar aquí...', autofocus='' v-model="searchQuery", @keyup.enter="search")
       .col.col-xs-1
         button.centrar.form-group.btn.btn-default(title='Buscar', @click='search')
           i.fa.fa-search.fa-lg
+      .col-xs-2.col-sm-4.col-md-4.margin-left
+        router-link(to="/home")
+          a.navbar-brand.navbar-right(@click='signOut')
+            strong.hidden-xs.margin &nbsp Salir 
+            i.fa.fa-sign-out
 </template>
 
 <script>
@@ -46,7 +51,22 @@
             customClass: 'animated tada'
           })
         }
+      },
+      signOut () {
+        window.localStorage.auth = ''
+        this.$router.push('/')
       }
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .margin-left{
+    margin-left: 20px
+  }
+  @media screen and (max-device-width : 480px) {
+    .margin-left{
+      margin-left: 30px
+    }
+  }
+</style>
--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,9 +1,34 @@
 <template lang="pug">
   nav.navbar-inverse.navbar-fixed-top.animated.fadeIn
-      .row.container-fluid
-        .col-xs-3.col-sm-4.col-md-5
-          router-link(to="/home")
-            a.navbar-brand
-              i.fa.fa-home.fa-fw
-              strong.hidden-xs &nbsp Inicio 
+    .row.container-fluid
+      .col-xs-10.col-sm-6.col-md-6
+        router-link(to="/home")
+          a.navbar-brand
+            i.fa.fa-home.fa-fw
+            strong.hidden-xs &nbsp Inicio 
+      .col-xs-2.col-sm-6.col-md-6
+        router-link(to="/home")
+          a.navbar-brand.navbar-right(@click='signOut')
+            strong.hidden-xs.margin &nbsp Salir 
+            i.fa.fa-sign-out.margin-right
 </template>
+
+<script>
+export default {
+  methods: {
+    signOut () {
+      window.localStorage.auth = ''
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .margin {
+    margin: 8px;
+  },
+  .margin-right {
+    margin-right: 10px;
+  }
+</style>
