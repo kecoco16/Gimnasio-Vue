4afdf62a2d16b752d47e2c2bb67022aa9417dcae
kecoco <kecoco16@gmail.com>
2018-02-02 12:01:02 -0600

    Agregando pantalla y pills de editar

---
+++ src/components/layout/edit/edit.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  main
+    gym-nav-bar
+    .box-principal
+      .animated.bounceInDown
+        gym-add-pills
+      gym-loader(v-show='isLoading')
+      div.row(v-show='!isLoading')
+    gym-footer(v-show='!isLoading')
+</template>
+
+<script>
+import GymLoader from '@/components/shared/Loader.vue'
+import GymFooter from '@/components/shared/Footer.vue'
+import GymAddPills from './editPills.vue'
+import GymNavBar from '@/components/shared/Header.vue'
+
+export default {
+  name: 'clients',
+
+  components: {GymLoader, GymFooter, GymAddPills, GymNavBar},
+
+  created () {
+  },
+  computed: {
+  }
+}
+</script>
+<style lang="scss">
+@media screen and (max-device-width : 480px) {
+  .modal-container{
+    width: auto;
+    margin:15px !important;
+    height: 0px auto;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 6px;
+  }
+}
+</style>
---
+++ src/components/layout/edit/editPills.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  main
+    h1.text-center
+      | Editar
+    ul.nav.nav-pills
+      li#pill1(@click='pill1', :class="pillsState[0]")
+        a Mensualidad
+      li#pill2(@click='pill2', :class="pillsState[1]")
+        a Usuario
+    hr.style-line
+</template>
+
+<script>
+  export default {
+    name: 'EditPills',
+    computed: {
+      pillsState () {
+        return this.$store.getters.pillsActive.map((state) => state ? 'active' : '')
+      }
+    },
+    methods: {
+      async pill1 () {
+        const pill1 = document.getElementById('pill1')
+        if (pill1.classList.value === 'active') { return }
+        this.$store.commit('isLoading', true)
+        this.$store.commit('clientsList', [])
+        this.$store.commit('pillActive', 'pill1')
+        const clients = await allClients.search()
+        if (clients) {
+          this.$store.commit('isLoading', false)
+          this.$store.commit('clientsList', clients)
+        }
+      },
+      async pill2 () {
+        const pill2 = document.getElementById('pill2')
+        if (pill2.classList.value === 'active') { return }
+        this.$store.commit('isLoading', true)
+        this.$store.commit('clientsList', [])
+        this.$store.commit('pillActive', 'pill2')
+        const clients = await todayClients.search()
+        if (clients) {
+          this.$store.commit('isLoading', false)
+          this.$store.commit('clientsList', clients)
+        }
+      }
+    }
+}
+</script> 
