faf881203e231ae8deee91ae7015213ba12e802b
Kevin Castillo Mora <kecoco16@gmail.com>
2018-08-05 09:13:42 -0600

    Refactor home page

--- src/components/shared/Footer.vue
+++ src/components/shared/Footer.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   footer.text-center
-      | © 2017 Hecho por 
-      a(href='https://www.facebook.com/kevin.castillo.39948', target='_blank') Kevin Castillo Mora 💻
+    | © 2017 Hecho por 
+    a(href='https://www.facebook.com/kevin.castillo.39948', target='_blank') Kevin Castillo Mora 💻
 </template>
 <style lang="css">
   footer {
